d7ef7f8d45bfb479639018e3c15330007f90f044
JP S <github@shipherd.com>
2020-10-04 17:33:54 -0400

    Use shard test modules for group and 1-1 tests

--- tests/test-jira-event-handler.js
+++ tests/test-jira-event-handler.js
@@ -4,6 +4,7 @@
 /*jshint esversion: 6 */  // Help out our linter
 
 // Helper classes for dealing with Jira Webhook payload
+require('dotenv').config();
 const JiraConnector = require('../jira-connector');
 const jiraConnector = new JiraConnector();
 const JiraEventHandler = require("../jira-event.js");
@@ -19,176 +20,35 @@ const test_dir = process.env.TEST_CASE_DIR;
 const JiraTestCases = require(`../${test_dir}/init-test-cases.js`);
 const jiraTestCases = new JiraTestCases;
 
-
-
-// Create our own verion of the Framework object that supports our test cases.
-function Framework() {
-  this.bots = [];
-}
-// jiraEventHandler calls framework.debug.   We don't care about this for our tests
-Framework.prototype.debug = function (message) {
-  if ((process.env.DEBUG) && (process.env.DEBUG.toLowerCase().substring('framework'))) {
-    console.log(message);
-  }
-};
 // Build the list of "bots" that we want our test suite to run against
 // The current set assumes all users work for ciso
+let Framework = require('./test-framework');
 framework = new Framework();
 jiraTestCases.initBotUsers(framework, test_dir);
 
 var testCases = [];
 jiraTestCases.initTestCases(testCases, test_dir, jiraConnector.getJiraUrl());
 
-// Run the Tests
+// Set VERBOSE=true to get test logging
 var verbose = false;
 if (process.env.VERBOSE) {
   verbose = true;
 }
 
-// This no longer works because it depends on each test completing before the next one starts
-// The new processJiraEvent does not complete the call to bot.say before the next test starts
-let expectedCallbacks = 0;
-for (var i = 0, len = testCases.length; i < len; i++) {
-  test = testCases[i];
-  expectedCallbacks += test.numExpectedResults;
-  //var jiraEvent = require(test.file);
-  var jiraEvent = JSON.parse(fs.readFileSync(`${test.file}`, "utf8"));
-  jiraEventHandler.processJiraEvent(jiraEvent, framework, checkTestResult(framework, test, i + 1));
-}
-
-// Set a timer to interrupt any long running tests.  Check which tests didn't get back expected results
-let timerDuration = expectedCallbacks * 1000;  // Allow 1 seconds per callback (or use environment)
-if (process.env.TEST_TIMER_MULTIPLIER) {
-  timerDuration = expectedCallbacks * parseInt(process.env.TEST_TIMER_MULTIPLIER);
-}
-console.log(`Running ${testCases.length} tests expected to generate ${expectedCallbacks} responses.`);
-console.log(`Set environment VERBOSE=true for more details.`);
-console.log(`Will analyze results in ${timerDuration / 1000} seconds...`); 
-setTimeout(() => {
-  let totalErrors = 0;
-  let totalPassed = 0;
-  for (var i = 0, len = testCases.length; i < len; i++) {
-    test = testCases[i];
-    totalErrors += test.numSeenErrors;
-    totalPassed += test.numPassed;
-    if ((test.result.length) && (test.result.length != test.numSeenErrors)) {
-      for (result of test.result) {
-        console.log(`Test ${i+1}: ${test.author} ${test.action} `+ 
-          `${test.subject}, based on file: ${test.file}, never got expected result:`);
-        console.log(result);
-        totalErrors += 1;
-      } 
-    }
-  }
-
-  console.log(`\nAll tests complete. ${totalPassed} tests passed.`);
-  if (totalErrors) {
-    console.error(`Number of errors seen: ${totalErrors}`);
-  }
-  process.exit();
-}, timerDuration);
-
-// Jira Event will call us back with the message that a bot sent
-// Check to see if matches our expected result
-function checkTestResult(framework, test, testNum) {
-  return function jiraEventCallback(err, bot = null) {
-    test.resultsSeen += 1;
-    if (verbose) {
-      console.log(`Checking a result for test ${testNum}: ${test.author} ` +
-        `${test.action} ${test.subject}, based on file: ${test.file}`);
-    }
-
-    if (err) {
-      console.error('Test %d (Result %d of %d) Failed.', testNum, test.numExpectedResults);
-      showExpected(`Got error in callback: ${err.message}`, test);
-      return;
-    }
-
-    //TODO figure out how to properly deal with this -- we got more results than expected!
-    if (!test.result.length) {
-      let seenResult = (bot) ? bot.jiraEventMessage : '';
-      console.error(`Already got all ${test.numExpectedResults} expected results for test `
-        `${testNum}. Extra result is: "${seenResult}"`);
-      test.numSeenErrors++;
-      return;
-    }
-
-    if (!bot) {
-      if (foundResult('', test)) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.  ` +
-          'Got expected non-notification');
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected('jiraEventHander did not find a bot for an intended recipent' +
-          ', but our expected results do not include an empty response.', test);
-      }
-      return;
-    }
-
-    var resultFound = false;
-    if (bot.jiraEventMessage) {
-      resultFound = true;
-      // Whitespace got me down, just removed it for this comparison
-      if (foundResult(bot.jiraEventMessage, test)) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.  ` +
-          'Got expected notification');
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected(`Got\n${bot.jiraEventMessage}`, test);
-      }
-      bot.jiraEventMessage = '';
-    }
-    if (!resultFound) {
-      if (!test.result) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.`);
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected('Got no result', test);
-      }
-    }
-  };
-}
+// Finally, load and initialize the class that will run each jira event based test
+let ProcessJiraTestCases = require('./process-jira-test-cases');
+let processJiraTests = new ProcessJiraTestCases(verbose);
+processJiraTests.runTests(testCases, jiraEventHandler, framework); 
 
 
-// Check the bot message against the set of valid results
-// We need to look at all possible values because the order of 
-// messages is not predictable since we are waiting on various
-// jira API calls to give us info about mentioned or watching users
-function foundResult(botMessage, test) {
-  for (let i=0; i<test.result.length; i++) {
-    // Strip whitespace to simply comparison
-    if (botMessage.replace(/\s/g, '') === test.result[i].replace(/\s/g, '')) {
-      // Remove this result from the set of expected result
-      test.result.splice(i, 1);
-      return true;
-    }
-  }
-  return false;
-}
+// // This no longer works because it depends on each test completing before the next one starts
+// // The new processJiraEvent does not complete the call to bot.say before the next test starts
+// let expectedCallbacks = 0;
+// for (var i = 0, len = testCases.length; i < len; i++) {
+//   test = testCases[i];
+//   expectedCallbacks += test.numExpectedResults;
+//   //var jiraEvent = require(test.file);
+//   var jiraEvent = JSON.parse(fs.readFileSync(`${test.file}`, "utf8"));
+//   jiraEventHandler.processJiraEvent(jiraEvent, framework, checkTestResult(framework, test, i + 1));
+// }
 
-// Function to display progress info
-function reportSuccess(test, msg) {
-  test.numPassed++;
-  if (verbose) { 
-    console.log(msg); 
-  }
-}
-
-
-// Function to display the expected results when a test fails
-function showExpected(msg, test) {
-  test.numSeenErrors += 1;
-  console.error(`While processing test input: ${test.file}`);
-  console.error(msg);
-  if (test.result.length == 1) {
-    console.error('Expected\n' + test.result[0]);
-  } else {
-    console.error('Expected one of:');
-    for(let i=0; i<test.result.length; i++) {
-      console.error(`${test.result[i]}`);
-    }
-  }
-}
--- tests/test-transition-notifications.js
+++ tests/test-transition-notifications.js
@@ -56,11 +56,6 @@ var verbose = false;
 if (process.env.VERBOSE) {
   verbose = true;
 }
-var myConsole = {};
-myConsole.log = function(msg) {
-  if (verbose) {console.log(msg);}
-}; 
-
 // Read in the configuration for our tests
 let conf = require(`./transition-test-config`);
 
@@ -120,6 +115,10 @@ if (conf?.testConfig?.firstPassTestCases?.length) {
   process.exit();
 }
 
+// Finally, load and initialize the class that will run each jira event based test
+let ProcessJiraTestCases = require('./process-jira-test-cases');
+let processJiraTests = new ProcessJiraTestCases(verbose);
+
 // Run the tests!
 // First the init cases validate that the "test bots" properly
 // handle the button presses to add boards to watch
@@ -139,7 +138,7 @@ runInitTestCases(initTestCases, groupNotifier)
     // Run the first pass of tests to check if the defined jira events
     // generate the expected notifications
     console.log('Ready to start notifying!');
-    return runTests(notifyTestCases, test_dir, jira, groupNotifier);
+    return processJiraTests.runTests(notifyTestCases, jiraEventHandler, framework); 
   })
   .then(() => {
     // If configured, emulate button presses to delete some of the boards
@@ -163,7 +162,7 @@ runInitTestCases(initTestCases, groupNotifier)
     if (conf?.testConfig?.secondPassTestCases?.length) {
       notifyTestCases = readNotifyTestCasesFromConfig(conf.testConfig.secondPassTestCases);
       console.log('Running notification tests again.');
-      return runTests(notifyTestCases, test_dir, jira, groupNotifier); 
+      return processJiraTests.runTests(notifyTestCases, jiraEventHandler, framework); 
     } else {
       console.error('No secondPassTestCases configured. Tests complete');
       process.exit();
@@ -375,290 +374,8 @@ function updateBotConfig(bot, board) {
     });
 }
 
-/**
- * Functions to send in the canned jira events and
- * process the results
- * 
- */
-async function runTests(testCases) {
-  // Loop through the test cases to and send each one to the event processor
-  let expectedCallbacks = 0;
-  for (var i = 0, len = testCases.length; i < len; i++) {
-    test = testCases[i];
-    expectedCallbacks += test.numExpectedResults;
-    var jiraEvent = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), test.file), "utf8"));
-    jiraEventHandler.processJiraEvent(jiraEvent, framework, checkTestResult(framework, test, i + 1));
-  }
-
-  // Set a timer to determine when to check if we missed any of the expected callbacks
-  let timerDuration = expectedCallbacks * 1000;  // Allow 1 seconds per callback (or use environment)
-  if (process.env.TEST_TIMER_MULTIPLIER) {
-    timerDuration = expectedCallbacks * parseInt(process.env.TEST_TIMER_MULTIPLIER);
-  }
-  myConsole.log(`Running ${testCases.length} tests expected to generate ${expectedCallbacks} responses.`);
-  myConsole.log(`Set environment VERBOSE=true for more details.`);
-  myConsole.log(`Will analyze results in ${timerDuration / 1000} seconds...`); 
-  // TODO -- modify this so it doesn't return until the promise gets
-  // resolved in the timeout handler
-  return new Promise((r) => returnAfterTimeout(r, testCases, timerDuration));
-}
-
-// Report test results after our timer expires
-function returnAfterTimeout(resolveMethod, testCases, timerDuration) {
-  setTimeout(() => {
-    let totalErrors = 0;
-    let totalPassed = 0;
-    for (var i = 0, len = testCases.length; i < len; i++) {
-      test = testCases[i];
-      totalErrors += test.numSeenErrors;
-      totalPassed += test.numPassed;
-      if ((test.result.length) && (test.result.length != test.numSeenErrors)) {
-        for (result of test.result) {
-          console.error(`Test ${i+1}: ${test.author} ${test.action} `+ 
-            `${test.subject}, based on file: ${test.file}, never got expected result:`);
-          console.error(result);
-          totalErrors += 1;
-        } 
-      }
-    }
-
-    myConsole.log(`\nAll tests complete. ${totalPassed} tests passed.`);
-    if (totalErrors) {
-      console.error(`Number of errors seen: ${totalErrors}`);
-    }
-    resolveMethod();
-  }, timerDuration);
-};
-
-// Jira event processor will call us back with the message that a bot sent
-// Check to see if matches our expected result
-function checkTestResult(framework, test, testNum) {
-  return function jiraEventCallback(err, bot = null) {
-    test.resultsSeen += 1;
-    if (verbose) {
-      myConsole.log(`Checking a result for test ${testNum}: ${test.author} ` +
-        `${test.action} ${test.subject}, based on file: ${test.file}`);
-    }
-
-    if (err) {
-      console.error('Test %d (Result %d of %d) Failed.', testNum, test.numExpectedResults);
-      showExpected(`Got error in callback: ${err.message}`, test);
-      return;
-    }
-
-    // Error if we got more results than expected.
-    if (!test.result.length) {
-      let seenResult = (bot) ? bot.jiraEventMessage : '';
-      console.error(`Already got all ${test.numExpectedResults} expected results for test `
-        `${testNum}. Extra result is: "${seenResult}"`);
-      test.numSeenErrors++;
-      return;
-    }
-
-    // No bot object means event processor found a user who could be notified
-    // but they do not have not registered a space with the bot
-    // (Configure users who have bots in init-test-cases.js in the TEST_CASE_DIR)
-    if (!bot) {
-      if (foundResult('', test)) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.  ` +
-          'Got expected non-notification');
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected('jiraEventHander did not find a bot for an intended recipent' +
-          ', but our expected results do not include an empty response.', test);
-      }
-      return;
-    }
-
-    // Compare result that bot "sent" is in the list of expected results in the test case
-    var resultFound = false;
-    if (bot.jiraEventMessage) {
-      resultFound = true;
-      // Whitespace got me down, just removed it for this comparison
-      if (foundResult(bot.jiraEventMessage, test)) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.  ` +
-          'Got expected notification');
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected(`Got\n${bot.jiraEventMessage}`, test);
-      }
-      bot.jiraEventMessage = '';
-    }
-    if (!resultFound) {
-      if (!test.result) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.`);
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected('Got no result', test);
-      }
-    }
-  };
-}
-
-
-// Check the bot message against the set of valid results
-// We need to look at all possible values because the order of 
-// messages is not predictable since we are waiting on various
-// jira API calls to give us info about mentioned or watching users
-function foundResult(botMessage, test) {
-  for (let i=0; i<test.result.length; i++) {
-    // Strip whitespace to simply comparison
-    if (botMessage.replace(/\s/g, '') === test.result[i].replace(/\s/g, '')) {
-      // Remove this result from the set of expected result
-      test.result.splice(i, 1);
-      return true;
-    }
-  }
-  return false;
-}
-
-// Function to display progress info
-function reportSuccess(test, msg) {
-  test.numPassed++;
-  if (verbose) { 
-    myConsole.log(msg); 
-  }
-}
-
-
-// Function to display the expected results when a test fails
-function showExpected(msg, test) {
-  test.numSeenErrors += 1;
-  console.error(`While processing test input: ${test.file}`);
-  console.error(msg);
-  if (test.result.length == 1) {
-    console.error('Expected\n' + test.result[0]);
-  } else {
-    console.error('Expected one of:');
-    for(let i=0; i<test.result.length; i++) {
-      console.error(`${test.result[i]}`);
-    }
-  }
-}
-
 function Promisedelay(t, val) {
   return new Promise(resolve => {
     setTimeout(resolve.bind(null, val), t);
   });
 };
-
-// Jira event processor will call us back with the message that a bot sent
-// Check to see if matches our expected result
-function checkTestResult(framework, test, testNum) {
-  return function jiraEventCallback(err, bot = null) {
-    test.resultsSeen += 1;
-    if (verbose) {
-      console.log(`Checking a result for test ${testNum}: ${test.author} ` +
-        `${test.action} ${test.subject}, based on file: ${test.file}`);
-    }
-
-    if (err) {
-      console.error('Test %d (Result %d of %d) Failed.', testNum, test.numExpectedResults);
-      showExpected(`Got error in callback: ${err.message}`, test);
-      return;
-    }
-
-    // Error if we got more results than expected.
-    if (!test.result.length) {
-      let seenResult = (bot) ? bot.jiraEventMessage : '';
-      if ((!bot) && (!seenResult)) {
-        if (verbose) {
-          console.error(`Already got all ${test.numExpectedResults} expected results for test ` +
-          `${testNum}, but saw an empty response. This can happen when new watchers were added ` +
-          `the issue since the test was originally created.  Ignoring.`);
-        }
-      } else {
-        console.error(`Already got all ${test.numExpectedResults} expected results for test ` +
-          `${testNum}. Extra result is: "${seenResult}"`);
-        test.numSeenErrors++;
-      }
-      return;
-    }
-
-    // No bot object means event processor found a user who could be notified
-    // but they do not have not registered a space with the bot
-    // (Configure users who have bots in init-test-cases.js in the TEST_CASE_DIR)
-    if (!bot) {
-      if (foundResult('', test)) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.  ` +
-          'Got expected non-notification');
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected('jiraEventHander did not find a bot for an intended recipent' +
-          ', but our expected results do not include an empty response.', test);
-      }
-      return;
-    }
-
-    // Compare result that bot "sent" is in the list of expected results in the test case
-    var resultFound = false;
-    if (bot.jiraEventMessage) {
-      resultFound = true;
-      // Whitespace got me down, just removed it for this comparison
-      if (foundResult(bot.jiraEventMessage, test)) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.  ` +
-          'Got expected notification');
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected(`Got\n${bot.jiraEventMessage}`, test);
-      }
-      bot.jiraEventMessage = '';
-    }
-    if (!resultFound) {
-      if (!test.result) {
-        reportSuccess(test, 
-          `Test ${testNum} (Result ${test.resultsSeen} of ${test.numExpectedResults}) Passed.`);
-      } else {
-        console.error('Test %d (Result %d of %d) Failed.', testNum, test.resultsSeen, test.numExpectedResults);
-        showExpected('Got no result', test);
-      }
-    }
-  };
-}
-
-
-// Check the bot message against the set of valid results
-// We need to look at all possible values because the order of 
-// messages is not predictable since we are waiting on various
-// jira API calls to give us info about mentioned or watching users
-function foundResult(botMessage, test) {
-  for (let i=0; i<test.result.length; i++) {
-    // Strip whitespace to simply comparison
-    if (botMessage.replace(/\s/g, '') === test.result[i].replace(/\s/g, '')) {
-      // Remove this result from the set of expected result
-      test.result.splice(i, 1);
-      return true;
-    }
-  }
-  return false;
-}
-
-// Function to display progress info
-function reportSuccess(test, msg) {
-  test.numPassed++;
-  if (verbose) { 
-    console.log(msg); 
-  }
-}
-
-
-// Function to display the expected results when a test fails
-function showExpected(msg, test) {
-  test.numSeenErrors += 1;
-  console.error(`While processing test input: ${test.file}`);
-  console.error(msg);
-  if (test.result.length == 1) {
-    console.error('Expected\n' + test.result[0]);
-  } else {
-    console.error('Expected one of:');
-    for(let i=0; i<test.result.length; i++) {
-      console.error(`${test.result[i]}`);
-    }
-  }
-}
-
